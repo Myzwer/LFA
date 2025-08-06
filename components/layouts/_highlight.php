3771cb51fefc9155822546e9465018bd64c973b0
Myzwer <josh@onefortyfivedesign.com>
2025-08-05 23:41:34 -0400

    :art: Update background color in highlights layout
    Changed the background color from black to olive for improved design consistency. This adjustment enhances the visual appeal in the highlights section.

--- components/layouts/_highlight.php
+++ components/layouts/_highlight.php
@@ -1,5 +1,5 @@
 <!-- Start Highlights -->
-<div class="bg-black text-white">
+<div class="bg-olive text-white">
     <div class="grid grid-cols-12 gap-4 py-5 md:py-0">
         <div class="col-span-12 md:col-span-4 md:border-r">
             <div class="text-center md:py-5 font-bold uppercase">
